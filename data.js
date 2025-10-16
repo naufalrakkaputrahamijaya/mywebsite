2daea804416e0bcec6a36ec6ec3989d0c822c39d
naufalrakkaputrahamijaya <naufalrakkaputrahamijaya@ugm.ac.id>
2025-10-16 11:23:34 +0700

    fungsi terpisah

---
+++ data.js
@@ -0,0 +1,11 @@
+const teamsData = [
+    { name: 'Geo Kusala', points: 13, scores: [3, 3, 3, 2, 2] },
+    { name: 'Aero Netra', points: 12, scores: [3, 3, 2, 1, 3] },
+    { name: 'Jalacise', points: 10, scores: [2, 2, 3, 3, 0] },
+    { name: 'Vajra Vium', points: 9, scores: [3, 0, 2, 2, 2] },
+    { name: 'Dhatu Stream', points: 7, scores: [1, 2, 1, 3, 0] },
+    { name: 'Infra Luhita', points: 6, scores: [0, 3, 1, 0, 2] },
+    { name: 'Loka Map', points: 4, scores: [1, 1, 0, 2, 0] },
+    { name: 'Nira Nautika', points: 3, scores: [0, 0, 0, 3, 0] },
+    { name: 'Giridera', points: 0, scores: [0, 0, 0, 0, 0] }
+];
