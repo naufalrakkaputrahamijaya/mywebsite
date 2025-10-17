085ccb8764ad666f93146baf7a445ba78ab7bc4b
naufalrakkaputrahamijaya <naufalrakkaputrahamijaya@ugm.ac.id>
2025-10-17 10:49:49 +0700

    baru

---
+++ klasemenPoin.js
@@ -0,0 +1,25 @@
+// Data poin klasemen untuk 9 tim
+const klasemenPoin = [
+  { name: 'Geo Kusala', poin: 64 },
+  { name: 'Aero Netra', poin: 55 },
+  { name: 'Jalacise', poin: 66 },
+  { name: 'Vajra Vium', poin: 92 },
+  { name: 'Dhatu Stream', poin: 47 },
+  { name: 'Infra Luhita', poin: 68 },
+  { name: 'Loka Map', poin: 82 },
+  { name: 'Nira Nautika', poin: 58 },
+  { name: 'Giridera', poin: 52 }
+];
+
+// Data detail penugasan untuk 9 tim
+const klasemenDetail = [
+  { name: 'Geo Kusala', tugas: [64, "Coming soon", "Coming soon", "Coming soon", "Coming soon"] },
+  { name: 'Aero Netra', tugas: [55, "Coming soon", "Coming soon", "Coming soon", "Coming soon"] },
+  { name: 'Jalacise', tugas: [66, "Coming soon", "Coming soon",  "Coming soon", "Coming soon"] },
+  { name: 'Vajra Vium', tugas: [92, "Coming soon", "Coming soon", "Coming soon", "Coming soon"] },
+  { name: 'Dhatu Stream', tugas: [47, "Coming soon", "Coming soon", "Coming soon", "Coming soon"] },
+  { name: 'Infra Luhita', tugas: [68, "Coming soon", "Coming soon", "Coming soon", "Coming soon"] },
+  { name: 'Loka Map', tugas: [82, "Coming soon", "Coming soon", "Coming soon", "Coming soon"] },
+  { name: 'Nira Nautika', tugas: [58, "Coming soon", "Coming soon", "Coming soon", "Coming soon"] },
+  { name: 'Giridera', tugas: [52, "Coming soon", "Coming soon", "Coming soon", "Coming soon"] }
+];
